759e2c5e65af217df303c03de9df827681652822
Faye Duxovni <duxovni@duxovni.org>
2015-11-03 15:05:59 -0500

    Added parsing to lyrics3v2.py, created module for parsing generic timed text

--- lyrics3v2.py
+++ lyrics3v2.py
@@ -1,11 +1,16 @@
 #! /usr/bin/python2
 
+import timedtext
+
 ID3_LENGTH = 128
 SIZE_LENGTH = 6
 START_TAG = "LYRICSBEGIN"
 END_TAG = "LYRICS200"
 
 SIZE_OFFSET = -(SIZE_LENGTH + len(END_TAG) + ID3_LENGTH)
+
+FIELD_ID_LENGTH = 3
+FIELD_SIZE_LENGTH = 5
 
 def read(filename):
     """Read Lyrics3 v2.00 data from an mp3 file
@@ -40,3 +45,64 @@ def write(filename, lyricsData):
             but not including size descriptor and "LYRICS200" string.
     """
     raise NotImplemented
+
+def load(lyricsData):
+    """Parse Lyrics3 v2.00 data
+
+    Args:
+        lyricsData (str): Lyrics3 v2.00 data, including "LYRICSBEGIN"
+            but not including size descriptor and "LYRICS200" string.
+
+    Returns:
+        New Lyrics instance containing the lyrics data
+
+    Raises:
+        ValueError: data isn't valid Lyrics3 v2.00 data or doesn't contain any
+            LYR field
+    """
+    if not lyricsData.startswith(START_TAG):
+        raise ValueError, "Not valid Lyrics3 v2.00 data"
+
+    lyricsData = lyricsData[len(START_TAG):]
+
+    lyrics = None
+    metadata = {}
+
+    while lyricsData:
+        fieldId = lyricsData[:FIELD_ID_LENGTH]
+        lyricsData = lyricsData[FIELD_ID_LENGTH:]
+
+        fieldSize = int(lyricsData[:FIELD_SIZE_LENGTH])
+        lyricsData = lyricsData[FIELD_SIZE_LENGTH:]
+
+        fieldData = lyricsData[:fieldSize]
+        lyricsData = lyricsData[fieldSize:]
+
+        if fieldId = "LYR":
+            lyrics = timedtext.load(fieldData)
+        elif fieldId == "EAL":
+            metadata["album"] = fieldData
+        elif fieldId = "EAR":
+            metadata["artist"] = fieldData
+        elif fieldId = "ETT":
+            metadata["title"] = fieldData
+        else:
+            pass
+
+    if lyrics is None:
+        raise ValueError, "Lyrics not found"
+
+    lyrics.setMetadata(**metadata)
+    return lyrics
+
+def dump(lyrics):
+    """Dump data from Lyrics instance to Lyrics3 v2.00 format
+
+    Args:
+        lyrics (Lyrics): Lyrics instance to dump as Lyrics3 v2.00 data
+
+    Returns:
+        str. String containing Lyrics3 v2.00 data, including "LYRICSBEGIN"
+            but not including size descriptor and "LYRICS200" string.
+    """
+    raise NotImplementedError
